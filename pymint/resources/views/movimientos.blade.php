f530abe80e3970e0a3a5251d73c087a782dabe3d
vmah <vavila1@me.com>
2021-02-28 01:11:16 -0600

    Agregando view de movimientos

--- pymint/resources/views/movimientos.blade.php
+++ pymint/resources/views/movimientos.blade.php
@@ -36,6 +36,39 @@
 @section("main_content")
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
 <h1 class="h3 mb-2 text-gray-800">Movimientos</h1>
-<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm"><i class="fas fa-plus"></i> Registrar Movimiento</a>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-info">
+                    Ingresos
+                </h6>
+            </div>
+            <div class="card-body">
+                <div class="text-center">
+                    <a href="#">
+                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;height: 100px;"src="/Fotos/ingresos.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-info">
+                    Gastos
+                </h6>
+            </div>
+            <div class="card-body">
+                <div class="text-center">
+                    <a href="#">
+                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;height: 100px;"src="/Fotos/gastos.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
